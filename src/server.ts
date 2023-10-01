33d9e66a4b6ab3d99438123ae5426395e87ed77d
vanei2002 <vanei.jesus016@gmail.com>
2023-10-01 00:33:01 -0300

    Starting project

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+// Import the framework and instantiate it
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', async function handler (request, reply) {
+  return { hello: 'world' }
+})
+
+// Run the server!
+try {
+  fastify.listen({ port: 3000 })
+} catch (err) {
+  fastify.log.error(err)
+}
